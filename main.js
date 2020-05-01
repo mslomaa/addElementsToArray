0079a425e2cfee985a44c79948293d7d96b1f84a
Mateusz Slomkowski <slomkowskimateusz@gmail.com>
2020-05-01 12:41:38 +0200

    working in code JS

--- main.js
+++ main.js
@@ -7,10 +7,16 @@ const btn = document.querySelector('button');
 btn.addEventListener('click', (e) => {
     e.preventDefault();
     const input = document.querySelector('input');
-    
-    const name = input.value;
-    names.push(name);
-    div.textContent += name + ', ';
-    input.value = '';
-
+    const newName = input.value;
+    if(input.value.length){
+        for(name of names){
+            if(name === newName){
+                alert('ta wartosc juz wystepuje');
+                return
+            }
+        }
+        names.push(newName);
+        div.textContent += newName + ', ';
+        input.value = '';
+    }
 })
